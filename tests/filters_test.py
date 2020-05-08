f8a4b84007d45459d9a46c53daee879318c7aced
a-n-rose <rose.aislyn.noelle@gmail.com>
2020-05-08 21:57:12 +0200

    added adjustment of frame length to setup_bands() test

--- tests/filters_test.py
+++ tests/filters_test.py
@@ -30,3 +30,23 @@ def test_setup_bands_8():
     value2 = np.array([120., 240., 360., 480., 600., 720., 840., 960.])
     assert np.array_equal(value1, band_start_freq)
     assert np.array_equal(value2, band_end_freq)
+
+def test_setup_bands_frameduration16ms():
+    fil = pyst.Filter(frame_duration = 16)
+    fil.setup_bands()
+    band_start_freq = fil.band_start_freq
+    band_end_freq = fil.band_end_freq
+    value1 = np.array([  0., 128., 256., 384., 512., 640.])
+    value2 = np.array([128., 256., 384., 512., 640., 768.])
+    assert np.array_equal(value1, band_start_freq)
+    assert np.array_equal(value2, band_end_freq)
+    
+def test_setup_bands_frameduration500ms():
+    fil = pyst.Filter(frame_duration = 500)
+    fil.setup_bands()
+    band_start_freq = fil.band_start_freq
+    band_end_freq = fil.band_end_freq
+    value1 = np.array([    0.,  4000.,  8000., 12000., 16000., 20000.])
+    value2 = np.array([ 4000.,  8000., 12000., 16000., 20000., 24000.])
+    assert np.array_equal(value1, band_start_freq)
+    assert np.array_equal(value2, band_end_freq)
