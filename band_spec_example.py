4f4e772bafc749d7286c8b5dfbda23fbe8a41fce
a-n-rose <rose.aislyn.noelle@gmail.com>
2020-05-11 00:28:52 +0200

    notes on application of phase. Applying it to original data, radians set to True sounds better upon first impression but both work! Yay!

--- band_spec_example.py
+++ band_spec_example.py
@@ -3,15 +3,17 @@ import pysoundtool as pyst
 
 output_wave_name1 = './test_bandspec1.wav'
 output_wave_name2 = './test_bandspec2.wav'
-target_wav = '/home/airos/Projects/Data/denoising_sample_data/noisy_speech_varied_IEEE_small/S_01_02__Water Cooler_scale0.3.wav'#'/home/airos/Projects/Data/denoising_sample_data/noisyspeech_uwnu_limit100_test/CHM02_56-08__noise_keyboard_scale0.3.wav'
-noise_file = None #'/home/airos/Projects/Data/sound/noize_examples/originals/air_conditioner/352807__bigmanjoe__fan.wav'#'/home/airos/Projects/Data/denoising_sample_data/noise_samples/noise_keyboard.wav'
+target_wav = '/home/airos/Projects/Data/denoising_sample_data/noisyspeech_uwnu_limit100_test/CHM02_56-08__noise_keyboard_scale0.3.wav' #'/home/airos/Projects/Data/denoising_sample_data/noisy_speech_varied_IEEE_small/S_01_02__Water Cooler_scale0.3.wav'
+noise_file = '/home/airos/Projects/Data/denoising_sample_data/noise_samples/noise_keyboard.wav'#'/home/airos/Projects/Data/sound/noize_examples/originals/air_conditioner/352807__bigmanjoe__fan.wav'#
 
+
+# phase radians set to True sounds much better.. I think. Point is, both work.
 pyst.apply_band_specsub(output_wave_name = output_wave_name1,
                         target_wav = target_wav,
                         noise_file = noise_file,
                         visualize=True,
-                        phase_radians=False,
-                        visualize_freq=50)
+                        phase_radians=True,
+                        visualize_freq=100)
 
 pyst.filtersignal(output_filename = output_wave_name2,
                         wavfile = target_wav,
