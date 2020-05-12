62e2d16d3a6018ccc2c3b741e8abc51b4f17834a
a-n-rose <rose.aislyn.noelle@gmail.com>
2020-05-13 01:16:47 +0200

    updated filters tests to updated filters functions - to working filters! Added numpy.random.seed() for generating random noise

--- tests/filters_test.py
+++ tests/filters_test.py
@@ -12,51 +12,52 @@ import pytest
 
 
 def test_setup_bands_default():
-    fil = pyst.Filter()
+    fil = pyst.BandSubtraction()
     fil.setup_bands()
     band_start_freq = fil.band_start_freq
     band_end_freq = fil.band_end_freq
-    value1 = np.array([  0., 160., 320., 480., 640., 800.])
-    value2 = np.array([160., 320., 480., 640., 800., 960.])
+    value1 = np.array([  0.,  80., 160., 240., 320., 400.])
+    value2 = np.array([ 80., 160., 240., 320., 400., 480.])
     assert np.array_equal(value1, band_start_freq)
     assert np.array_equal(value2, band_end_freq)
     
 def test_setup_bands_8():
-    fil = pyst.Filter(num_bands = 8)
+    fil = pyst.BandSubtraction(num_bands = 8)
     fil.setup_bands()
     band_start_freq = fil.band_start_freq
     band_end_freq = fil.band_end_freq
-    value1 = np.array([  0., 120., 240., 360., 480., 600., 720., 840.])
-    value2 = np.array([120., 240., 360., 480., 600., 720., 840., 960.])
+    value1 = np.array([  0.,  60., 120., 180., 240., 300., 360., 420.])
+    value2 = np.array([ 60., 120., 180., 240., 300., 360., 420., 480.])
     assert np.array_equal(value1, band_start_freq)
     assert np.array_equal(value2, band_end_freq)
 
 def test_setup_bands_frameduration16ms():
-    fil = pyst.Filter(frame_duration = 16)
+    fil = pyst.BandSubtraction(frame_duration_ms = 16)
     fil.setup_bands()
     band_start_freq = fil.band_start_freq
     band_end_freq = fil.band_end_freq
-    value1 = np.array([  0., 128., 256., 384., 512., 640.])
-    value2 = np.array([128., 256., 384., 512., 640., 768.])
+    value1 = np.array([  0.,  64., 128., 192., 256., 320.])
+    value2 = np.array([ 64., 128., 192., 256., 320., 384.])
     assert np.array_equal(value1, band_start_freq)
     assert np.array_equal(value2, band_end_freq)
     
 def test_setup_bands_frameduration500ms():
-    fil = pyst.Filter(frame_duration = 500)
+    fil = pyst.BandSubtraction(frame_duration_ms = 500)
     fil.setup_bands()
     band_start_freq = fil.band_start_freq
     band_end_freq = fil.band_end_freq
-    value1 = np.array([    0.,  4000.,  8000., 12000., 16000., 20000.])
-    value2 = np.array([ 4000.,  8000., 12000., 16000., 20000., 24000.])
+    value1 = np.array([    0.,  2000.,  4000.,  6000.,  8000., 10000.])
+    value2 = np.array([ 2000.,  4000.,  6000.,  8000., 10000., 12000.])
     assert np.array_equal(value1, band_start_freq)
     assert np.array_equal(value2, band_end_freq)
     
 def test_update_posteri_bands_noisy():
     noise_max = 0.3
-    fil = pyst.Filter(num_bands = 3)
+    fil = pyst.BandSubtraction(num_bands = 3)
     fil.setup_bands()
     time = np.arange(0, 10, 0.01)
     signal = np.sin(time)[:fil.frame_length]
+    np.random.seed(seed=0)
     noise = np.random.normal(np.mean(signal),
                              np.mean(signal)+noise_max,
                              fil.frame_length)
@@ -64,16 +65,17 @@ def test_update_posteri_bands_noisy():
     powspec_noisy = np.abs(np.fft.fft(signal + noise))**2
     fil.update_posteri_bands(powspec, powspec_noisy)
     snr_bands = fil.snr_bands
-    value1 = np.array([ -2.38856707, -45.41496251,  -1.48348645])
+    value1 = np.array([ -2.02865226, -41.70672353, -45.45654087])
     assert np.allclose(value1, snr_bands)
     
 
 def test_update_posteri_bands_verynoisy():
     noise_max = 0.7
-    fil = pyst.Filter(num_bands = 3)
+    fil = pyst.BandSubtraction(num_bands = 3)
     fil.setup_bands()
     time = np.arange(0, 10, 0.01)
     signal = np.sin(time)[:fil.frame_length]
+    np.random.seed(seed=0)
     noise = np.random.normal(np.mean(signal),
                              np.mean(signal)+noise_max,
                              fil.frame_length)
@@ -81,11 +83,11 @@ def test_update_posteri_bands_verynoisy():
     powspec_noisy = np.abs(np.fft.fft(signal + noise))**2
     fil.update_posteri_bands(powspec, powspec_noisy)
     snr_bands = fil.snr_bands
-    value1 = np.array([ -3.94990272, -50.07895978,  -3.31517877])
+    value1 = np.array([ -2.82864994, -46.76075799, -50.50670912])
     assert np.allclose(value1, snr_bands)
     
 def test_update_posteri_bands_nonoise():
-    fil = pyst.Filter(num_bands = 3)
+    fil = pyst.BandSubtraction(num_bands = 3)
     fil.setup_bands()
     time = np.arange(0, 10, 0.01)
     signal = np.sin(time)[:fil.frame_length]
@@ -98,10 +100,11 @@ def test_update_posteri_bands_nonoise():
     
 def test_calc_oversub_factor_noisy():
     noise_max = 0.3
-    fil = pyst.Filter(num_bands = 4)
+    fil = pyst.BandSubtraction(num_bands = 4)
     fil.setup_bands()
     time = np.arange(0, 10, 0.01)
     signal = np.sin(time)[:fil.frame_length]
+    np.random.seed(seed=0)
     noise = np.random.normal(np.mean(signal),
                              np.mean(signal)+noise_max,
                              fil.frame_length)
@@ -109,12 +112,12 @@ def test_calc_oversub_factor_noisy():
     powspec_noisy = np.abs(np.fft.fft(signal + noise))**2
     fil.update_posteri_bands(powspec, powspec_noisy)
     a = fil.calc_oversub_factor()
-    value1 = np.array([4.33042222, 4.75,       4.75,       4.16179247])
+    value1 = np.array([4.28678354, 4.75,       4.75,       4.75      ])
     assert np.allclose(value1, a)
     
 def test_calc_oversub_factor_nonoise():
     noise_max = 0.3
-    fil = pyst.Filter(num_bands = 4)
+    fil = pyst.BandSubtraction(num_bands = 4)
     fil.setup_bands()
     time = np.arange(0, 10, 0.01)
     signal = np.sin(time)[:fil.frame_length]
@@ -125,7 +128,7 @@ def test_calc_oversub_factor_nonoise():
     assert np.allclose(value1, a)
     
 def test_calc_relevant_band1():
-    fil = pyst.Filter(num_bands = 6)
+    fil = pyst.BandSubtraction(num_bands = 6)
     fil.setup_bands()
     band_index = 0
     freq = fil.band_start_freq[band_index] 
@@ -148,7 +151,7 @@ def test_calc_relevant_band1():
     
     
 def test_calc_relevant_band2():
-    fil = pyst.Filter(num_bands = 6)
+    fil = pyst.BandSubtraction(num_bands = 6)
     fil.setup_bands()
     band_index = 1
     freq = fil.band_start_freq[band_index] 
@@ -171,7 +174,7 @@ def test_calc_relevant_band2():
     
     
 def test_calc_relevant_band4():
-    fil = pyst.Filter(num_bands = 6)
+    fil = pyst.BandSubtraction(num_bands = 6)
     fil.setup_bands()
     band_index = 2
     freq = fil.band_start_freq[band_index] 
@@ -193,7 +196,7 @@ def test_calc_relevant_band4():
     assert value1 == rel_band
     
 def test_calc_relevant_band4():
-    fil = pyst.Filter(num_bands = 6)
+    fil = pyst.BandSubtraction(num_bands = 6)
     fil.setup_bands()
     band_index = 3
     freq = fil.band_start_freq[band_index]
@@ -215,7 +218,7 @@ def test_calc_relevant_band4():
     assert value1 == rel_band
     
 def test_calc_relevant_band():
-    fil = pyst.Filter(num_bands = 4)
+    fil = pyst.BandSubtraction(num_bands = 4)
     fil.setup_bands()
     time = np.arange(0, 10, 0.01)
     signal = np.cos(time)[:fil.frame_length]
@@ -223,4 +226,25 @@ def test_calc_relevant_band():
     rel_band, pow_levels = fil.calc_relevant_band(powspec)
     value1 = 0
     assert value1 == rel_band
+    
+#def test_sub_noise():
+    #fil = pyst.BandSubtraction(num_bands = 4)
+    #fil.setup_bands()
+    #time = np.arange(0, 10, 0.01)
+    #signal = np.sin(time)[:fil.frame_length]
+    #powspec = np.abs(np.fft.fft(signal))**2
+    ## add noise
+    #np.random.seed(seed=0)
+    #noise = 0.1 * np.random.randn(len(signal))
+    #noisy_signal = signal + noise
+    #powspec_noisy = np.abs(np.fft.fft(noisy_signal))**2
+    ## calculate other necessary variables
+    #fil.update_posteri_bands(powspec, powspec_noisy)
+    #a = fil.calc_oversub_factor()
+    #sub_signal = fil.sub_noise(powspec, powspec_noisy, 
+                               #oversub_factor = a,
+                               #speech = True)
+
+    
+    
     
