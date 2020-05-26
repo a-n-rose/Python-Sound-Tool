bug fix: longer duration and non normed samples result in silent wavfile. Removed option to not normalize (between 1 and -1) sound samples; performs this automatically

pysoundtool/dsp.py
@@ -86,15 +86,12 @@ def set_signal_length(samples, numsamps):
     data = samples.copy()
     if data.shape[0] < numsamps:
         diff = numsamps - data.shape[0]
-        # set zeros to mean values
-        mean_value = int(np.mean(data))
         if len(data.shape) > 1:
             signal_zeropadded = np.zeros(
                 (data.shape[0] + int(diff),data.shape[1]))
         else:
             signal_zeropadded = np.zeros(
                 (data.shape[0] + int(diff),))
-        signal_zeropadded += mean_value
         for i, row in enumerate(data):
             signal_zeropadded[i] = row
         data = signal_zeropadded
@@ -105,7 +102,7 @@ def set_signal_length(samples, numsamps):
             data = data[:numsamps]
     return data
 
-def loadsound(filename, sr=None, norm=True, mono=True, dur_sec = None):
+def loadsound(filename, sr=None, mono=True, dur_sec = None):
     '''Loads sound file with scipy.io.wavfile.read
     
     If the sound file is not compatible with scipy's read module
@@ -119,8 +116,6 @@ def loadsound(filename, sr=None, norm=True, mono=True, dur_sec = None):
     sr : int, optional
         The desired sample rate of the audio samples. If None, 
         the sample rate of the audio file will be used.
-    norm : bool
-        If True, the samples will be normlized between -1 and 1
     mono : bool
         If True, the samples will be loaded in mono sound. If False,
         if the samples are in stereo, they will be loaded in stereo sound.
@@ -130,7 +125,8 @@ def loadsound(filename, sr=None, norm=True, mono=True, dur_sec = None):
     Returns
     -------
     data : nd.array [size=(num_samples,) or (num_samples, num_channels)]
-        The sample data returned according to the specified settings.
+        The normalized (between 1 and -1) sample data returned 
+        according to the specified settings.
     sr : int 
         The sample rate of the loaded samples.
     '''
@@ -158,13 +154,11 @@ def loadsound(filename, sr=None, norm=True, mono=True, dur_sec = None):
     if mono and len(data.shape) > 1:
         if data.shape[1] > 1:
             data = pyst.tools.stereo2mono(data)
-    if norm:
-        data = pyst.tools.normsound(data, -1, 1)
+    # normalize samples
+    data = pyst.tools.normsound(data, -1, 1)
     if dur_sec:
         numsamps = int(dur_sec * sr)
-    else:
-        numsamps = data.shape[0]
-    data = set_signal_length(data, numsamps)
+        data = set_signal_length(data, numsamps)
     return data, sr
 
 def normalize(data, max_val=None, min_val=None):