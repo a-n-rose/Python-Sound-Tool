3f824b2393c0c7276f17cf9fb212eccb5d907752
a-n-rose <rose.aislyn.noelle@gmail.com>
2020-05-29 18:40:44 +0200

    moved loadsound to data.py module

--- pysoundtool/__init__.py
+++ pysoundtool/__init__.py
@@ -11,10 +11,11 @@ from .filters import calc_audioclass_powerspecs as welch2class
 from .filters import coll_beg_audioclass_samps as save_class_noise
 from . import dsp
 from . import exceptions as errors
-from .dsp import loadsound
+from .data import loadsound
+from . import data
 
 __all__=['paths', 'PathSetup', 'utils', 'feats',\
      'FeatPrep_SoundClassifier', 'prepfeatures','getfeatsettings',\
         'filters','WienerFilter','BandSubtraction','welch2class',\
             'save_class_noise','filtersignal', \
-             'dsp','errors', 'visualize_feats', 'loadsound']
+             'dsp','errors', 'visualize_feats', 'loadsound', 'data']
