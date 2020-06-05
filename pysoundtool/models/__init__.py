924f3d6f50ccbce8135072733bd4593281577a48
a-n-rose <rose.aislyn.noelle@gmail.com>
2020-06-06 00:34:21 +0200

    import basic models

--- pysoundtool/models/__init__.py
+++ pysoundtool/models/__init__.py
@@ -1,5 +1,6 @@
 from .cnn import SoundClassifier, ClassifySound, buildclassifier, loadclassifier
 from . import generator
+from .template_models import cnn_classifier, autoencoder_denoise
 
 __all__ = ['SoundClassifier', 'ClassifySound', 'buildclassifier', 'loadclassifier', 
-           'generator']
+           'generator', 'cnn_classifier', 'autoencoder_denoise']
