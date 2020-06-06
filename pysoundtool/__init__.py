86562b4266ee1d46fdc19ced89f6aa2d998848f2
a-n-rose <rose.aislyn.noelle@gmail.com>
2020-06-06 12:58:53 +0200

    adjusted names: visualize_feats --> pyst.feats.plot; visualize_audio --> pyst.plotsound

--- pysoundtool/__init__.py
+++ pysoundtool/__init__.py
@@ -4,7 +4,7 @@ from .paths import PathSetup
 from .utils import utils
 from . import feats
 from .feats import FeatPrep_SoundClassifier, prepfeatures, getfeatsettings, \
-    visualize_feats
+    plotsound
 from . import filters
 from .filters import WienerFilter, BandSubtraction, filtersignal
 from .filters import calc_audioclass_powerspecs as welch2class
@@ -18,4 +18,4 @@ __all__=['paths', 'PathSetup', 'utils', 'feats',\
      'FeatPrep_SoundClassifier', 'prepfeatures','getfeatsettings',\
         'filters','WienerFilter','BandSubtraction','welch2class',\
             'save_class_noise','filtersignal', \
-             'dsp','errors', 'visualize_feats', 'loadsound', 'data']
+             'dsp','errors', 'plotsound', 'loadsound', 'data']
