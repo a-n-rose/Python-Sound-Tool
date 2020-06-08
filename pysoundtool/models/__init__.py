e1837ae33a805bf7e200a8a4c1c32ca6e4e0655a
a-n-rose <rose.aislyn.noelle@gmail.com>
2020-06-08 18:26:39 +0200

    add Generator and setup_callbacks

--- pysoundtool/models/__init__.py
+++ pysoundtool/models/__init__.py
@@ -1,6 +1,7 @@
 from .cnn import SoundClassifier, ClassifySound, buildclassifier, loadclassifier
-from . import generator
+from .generator import Generator
 from .template_models import cnn_classifier, autoencoder_denoise
+from .modelsetup import setup_callbacks
 
 __all__ = ['SoundClassifier', 'ClassifySound', 'buildclassifier', 'loadclassifier', 
-           'generator', 'cnn_classifier', 'autoencoder_denoise']
+           'Generator', 'cnn_classifier', 'autoencoder_denoise', 'setup_callbacks']
