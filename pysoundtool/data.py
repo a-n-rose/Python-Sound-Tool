61c3acd333adc2ab8973d6d2e11298c36b64da3f
a-n-rose <rose.aislyn.noelle@gmail.com>
2020-06-10 16:41:08 +0200

    removed unnecessary line

--- pysoundtool/data.py
+++ pysoundtool/data.py
@@ -17,7 +17,6 @@ import os, sys
 import inspect
 currentdir = os.path.dirname(os.path.abspath(
     inspect.getfile(inspect.currentframe())))
-#parentdir = os.path.dirname(currentdir)
 packagedir = os.path.dirname(currentdir)
 sys.path.insert(0, packagedir)
 
