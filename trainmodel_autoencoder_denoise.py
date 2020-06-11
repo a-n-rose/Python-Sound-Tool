70f56e052dc1d25b43dfd68935b45fa302a9a233
a-n-rose <rose.aislyn.noelle@gmail.com>
2020-06-11 18:42:45 +0200

    added csv dict with info about model and training

--- trainmodel_autoencoder_denoise.py
+++ trainmodel_autoencoder_denoise.py
@@ -4,13 +4,19 @@ import pathlib
 import pysoundtool as pyst
 import pysoundtool.models as soundmodels
 import keras
+import time
 
 
 ###############################################################################
 
 # make sure data files exists:
+model_name = 'autoencoder_denoise'
 dataset_path = pyst.utils.check_dir('./audiodata/test_denoiser/', make = False) 
 feature_type = 'stft'
+model_name += '_'+feature_type + '_' + pyst.utils.get_date() + '.h5'
+
+model_path = dataset_path.joinpath(model_name)
+
 data_train_noisy_path = dataset_path.joinpath('train_data_noisy_{}.npy'.format(feature_type))
 data_val_noisy_path = dataset_path.joinpath('val_data_noisy_{}.npy'.format(
     feature_type))
@@ -46,7 +52,8 @@ print('input shape: ', input_shape)
 
 denoiser = soundmodels.autoencoder_denoise(input_shape = input_shape)
 
-callbacks = soundmodels.setup_callbacks(patience=5)
+callbacks = soundmodels.setup_callbacks(patience=5,
+                                        best_modelname=model_path)
 adm = keras.optimizers.Adam(learning_rate=0.0001)
 denoiser.compile(optimizer=adm, loss='binary_crossentropy')
 
@@ -55,8 +62,10 @@ data_val_noisy = np.load(data_val_noisy_path)
 
 data_train_clean = np.load(data_train_clean_path)
 data_val_clean = np.load(data_val_clean_path)
-
     
+    
+start = time.time()
+
 if use_generator:
     train_generator = soundmodels.Generator(data_matrix1 = data_train_noisy, 
                                             data_matrix2 = data_train_clean,
@@ -89,3 +98,16 @@ else:
                                    batch_size = data_train_noisy.shape[1],
                                    callbacks = callbacks, 
                                    validation_data = (X_val, y_val))
+end = time.time()
+total_dur_sec = round(end-start,2)
+model_features_dict = dict([('model', model_path),
+                            ('train_noisy', data_train_noisy_path),
+                            ('val_noisy', data_val_noisy_path),
+                            ('train_clean', data_train_clean_path),
+                            ('val_clean', data_val_clean_path),
+                            ('train_duration_seconds', total_dur_sec)])
+
+model_features_dict_path = dataset_path.joinpath('info_{}.csv'.format(
+    model_name))
+model_features_dict_path = pyst.utils.save_dict(model_features_dict,
+                                                model_features_dict_path)
