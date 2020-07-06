6dd6f67180f3d9a353b46a95fa4f97644779294f
a-n-rose <rose.aislyn.noelle@gmail.com>
2020-07-06 20:22:37 +0200

    added function for plotting cnn layer feature maps

--- pysoundtool/models/__init__.py
+++ pysoundtool/models/__init__.py
@@ -1,6 +1,7 @@
 from .dataprep import Generator
 from .template_models import cnn_classifier, autoencoder_denoise
 from .modelsetup import setup_callbacks
+from . import plot
 
 __all__ = ['Generator', 'cnn_classifier', 'autoencoder_denoise',
-           'setup_callbacks']
+           'setup_callbacks', 'plot']
