1101d332aef29fec01219cb311765acc35227452
a-n-rose <rose.aislyn.noelle@gmail.com>
2020-07-20 21:21:27 +0200

    debugged setup.py for pypi package creation

--- setup.py
+++ setup.py
@@ -7,15 +7,14 @@ HERE = pathlib.Path(__file__).parent
 # The text of the README file
 README = (HERE / "README.md").read_text()
 
-def parse_requirements(filename):
-    """Load requirements from a pip requirements file."""
-    lineiter = (line.strip() for line in open(filename))
-    return [line for line in lineiter if line and not line.startswith("#")]
+dependencies=''
+with open("requirements.txt","r") as f:
+        dependencies = f.read().splitlines()
 
 # This call to setup() does all the work
 setup(
     name="pysoundtool",
-    version="0.1.0a",
+    version="0.1.0a1",
     description="A research-based framework for exploring sound as well as machine learning in the context of sound.",
     long_description=README,
     long_description_content_type="text/markdown",
@@ -24,12 +23,12 @@ setup(
     author_email="rose.aislyn.noelle@gmail.com",
     license="AGPL-3.0",
     classifiers=[
-        "License :: OSI Approved :: GNU Affero General Public License",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
     ],
     packages=find_packages(exclude=("tests","docs","pysoundtool_online", "binder_notebooks", "jupyter_notebooks")),
     include_package_data=True,
-    install_requires=parse_requirements('requirements.txt'),
-    python_requires="==3.6",
+    install_requires=dependencies,
+    python_requires="==3.6.9",
 )
