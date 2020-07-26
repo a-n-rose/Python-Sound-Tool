44eb3b484b51235f0e56e8c1d691f035eb118b39
a-n-rose <rose.aislyn.noelle@gmail.com>
2020-07-27 01:15:25 +0200

    expanded on add_backgroundsound docstring; moved get_vad_samples and get_vad_stft to feats module for consistency

--- pysoundtool/dsp.py
+++ pysoundtool/dsp.py
@@ -367,10 +367,10 @@ def stereo2mono(data):
         data_mono = np.take(data,0,axis=-1) 
     return data_mono
 
-def add_backgroundsound(audio_main, audio_background, sr, snr=None, 
+def add_backgroundsound(audio_main, audio_background, sr, snr = None, 
                         delay_mainsound_sec = None, total_len_sec=None,
                         wrap = False, stationary_noise = True, 
-                        random_seed = None, extend_window_ms=0, **kwargs):
+                        random_seed = None, extend_window_ms = 0, **kwargs):
     '''Adds a sound (i.e. background noise) to a target signal.
     
     If the sample rates of the two audio samples do not match, the sample
@@ -389,12 +389,14 @@ def add_backgroundsound(audio_main, audio_background, sr, snr=None,
         string, should be a data samples corrresponding to the provided sample rate.
         
     sr : int 
-        The sample rate of sounds to be added together.
+        The sample rate of sounds to be added together. Note: sr of 44100 or higher
+        is suggested.
     
     snr : int, float
         The sound-to-noise-ratio of the target and background signals. Note: this
         is an approximation and needs further testing and development to be 
-        used as an official measurement of snr.
+        used as an official measurement of snr. If no SNR provided, signals 
+        will be added together as-is. (default None)
     
     delay_mainsound_sec : int or float, optional
         Length of time in seconds the main sound will be delayed. For example, 
@@ -411,9 +413,23 @@ def add_backgroundsound(audio_main, audio_background, sr, snr=None,
         the end of the audio file. If True, the random selection will wrap to 
         beginning of the audio file if extends beyond the end of the audio file.
         (default False)
+        
+    stationary_noise : bool 
+        If False, `pysoundtool.dsp.get_vad_stft` will be applied to noise to get 
+        energy of the active noise in the signal. Otherwise energy will be 
+        collected via `pysoundtool.dsp.get_stft`. (default True)
+        
+    random_seed : int 
+        If provided, the 'random' section of noise will be chosen using this seed.
+        (default None)
+        
+    extend_window_ms : int or float
+        The number of milliseconds the voice activity detected should be padded with.
+        This might be useful to ensure sufficient amount of activity is calculated.
+        (default 0) 
     
     **kwargs : additional keyword arguments
-        The keyword arguments for pysoundtool.files.loadsound
+        The keyword arguments for pysoundtool.files.loadsound (Note: )
     
     
     Returns
@@ -441,7 +457,19 @@ def add_backgroundsound(audio_main, audio_background, sr, snr=None,
         
     pysoundtool.dsp.snr_adjustnoiselevel
         Calculates how much to adjust noise signal to achieve SNR.
+        
+    pysoundtool.feats.get_vad_stft
+        Returns stft matrix of only voice active regions
+        
+    pysoundtool.feats.get_stft 
+        Returns stft matrix of entire signal
+        
     '''
+    if sr < 44100:
+        import warnings
+        msg = 'Performance of signal to noise analysis is improved with '+\
+            'sample rates at or higher than 44100 Hz. Current sample rate '+\
+                'set at {}.'.format(sr)
     input_type_main = pyso.utils.path_or_samples(audio_main)
     input_type_background = pyso.utils.path_or_samples(audio_background)
     if 'path' in input_type_main:
@@ -465,7 +493,7 @@ def add_backgroundsound(audio_main, audio_background, sr, snr=None,
         else:
             num_channels = 1
         sound2add = apply_num_channels(sound2add, num_channels)
-    target_stft, __ = pyso.dsp.get_vad_stft(target, sr, 
+    target_stft, __ = pyso.feats.get_vad_stft(target, sr, 
                                             extend_window_ms = extend_window_ms)
     if not target_stft.any():
         import warnings
@@ -476,7 +504,7 @@ def add_backgroundsound(audio_main, audio_background, sr, snr=None,
         noise_stft = pyso.feats.get_stft(sound2add, sr)
     else:
         # get energy of noise when active (e.g. car honking)
-        noise_stft, __ = pyso.dsp.get_vad_stft(sound2add, sr,
+        noise_stft, __ = pyso.feats.get_vad_stft(sound2add, sr,
                                                extend_window_ms = extend_window_ms)
         if not noise_stft.any():
             noise_stft = pyso.feats.get_stft(sound2add, sr)
@@ -1368,7 +1396,7 @@ def get_vad_snr(target_samples, noise_samples, sr, extend_window_ms = 0):
     https://www.who.int/occupational_health/publications/noise1.pdf
     '''
     # get target power with only high energy values (vad)
-    vad_stft, __ = pyso.dsp.get_vad_stft(target_samples, 
+    vad_stft, __ = pyso.feats.get_vad_stft(target_samples, 
                                        sr=sr, extend_window_ms = extend_window_ms)
     if not vad_stft.any():
         import warnings
@@ -1383,85 +1411,7 @@ def get_vad_snr(target_samples, noise_samples, sr, extend_window_ms = 0):
     snr = np.mean(snr)
     return snr
 
-# not working well
-def get_vad_samples(sound, sr=44100, win_size_ms = 50, percent_overlap = 0,
-                    use_beg_ms = 120, extend_window_ms=0):
-    # resample data if sr < 44100
-    if isinstance(sound, np.ndarray):
-        data = sound
-        if sr < 44100:
-            import warnings
-            msg = '\nWarning: VAD works best with sample rates at or above '+\
-                '44100 Hz. Therefore, audio will be sampled / resampled from '+\
-                    ' {} to 44100 Hz.'.format(sr)
-            warnings.warn(msg)
-            data, sr = pyso.dsp.resample_audio(data, sr, 44100)
-    else:
-        if sr < 44100:
-            import warnings
-            msg = '\nWarning: VAD works best with sample rates at or above '+\
-                '44100 Hz. Therefore, audio will be sampled / resampled from '+\
-                    ' {} to 44100 Hz.'.format(sr)
-            warnings.warn(msg)
-            sr = 44100
-        data, sr2 = pyso.loadsound(sound, sr=sr)
-        assert sr2 == sr
-        
-    frame_length = pyso.dsp.calc_frame_length(win_size_ms, sr)
-    if percent_overlap > 0:
-        import warnings
-        msg = 'For VAD calculation, no overlap applied. '+\
-            'Therefore, `percent_overlap` set to 0.'
-        warnings.warn(msg)
-        percent_overlap = 0
-    num_overlap_samples = int(frame_length * percent_overlap)
-    num_subframes = pyso.dsp.calc_num_subframes(len(data),
-                                                frame_length = frame_length,
-                                                overlap_samples = num_overlap_samples,
-                                                zeropad = True)
-    # set number of subframes for extending window
-    extwin_num_samples = pyso.dsp.calc_frame_length(extend_window_ms, sr)
-    num_win_subframes = pyso.dsp.calc_num_subframes(extwin_num_samples,
-                                                    frame_length = frame_length,
-                                                    overlap_samples = num_overlap_samples,
-                                                    zeropad = True)
-    
-    samples_matrix = pyso.dsp.create_empty_matrix((len(data)),
-                                                complex_vals = False)
-    vad_matrix, (sr, e, f, sfm) = pyso.dsp.vad(data, sr, 
-                                               win_size_ms = win_size_ms,
-                                               percent_overlap = percent_overlap, 
-                                               use_beg_ms = use_beg_ms)
-    vad_matrix_extwin = vad_matrix.copy()
-    # extend VAD windows with 1s
-    if extend_window_ms > 0:
-        for i, row in enumerate(vad_matrix):
-            if row > 0:
-                # label samples before VAD as VAD
-                if i > num_win_subframes:
-                    vad_matrix_extwin[i-num_win_subframes:i] = 1
-                else:
-                    vad_matrix_extwin[:i] = 1
-                # label samples before VAD as VAD
-                if i + num_win_subframes < len(vad_matrix):
-                    vad_matrix_extwin[i:num_win_subframes+i] = 1
-                else:
-                    vad_matrix_extwin[i:] = 1
-            
-    section_start = 0
-    extra_rows = 0
-    row = 0
-    for frame in range(num_subframes):
-        vad = vad_matrix_extwin[frame]
-        if vad > 0:
-            section = data[section_start:section_start+frame_length]
-            samples_matrix[row:row+len(section)] = section
-            row += len(section)
-        else:
-            extra_rows += frame_length
-        section_start += (frame_length - num_overlap_samples)
-    samples_matrix = samples_matrix[:-extra_rows]
-    return samples_matrix, sr
+
 
 # Not having success with this
 def snr_adjustnoiselevel(target_samples, noise_samples, sr, snr):
@@ -2193,99 +2143,6 @@ def get_pitch(sound, sr=16000, win_size_ms = 50, percent_overlap = 0.5,
         section_start += (frame_length - num_overlap_samples)
     return freq_matrix
     
-def get_vad_stft(sound, sr=44100, win_size_ms = 50, percent_overlap = 0,
-                          real_signal = False, fft_bins = 1024, 
-                          window = 'hann', use_beg_ms = 120,
-                          extend_window_ms = 0):
-    # resample data if sr < 44100
-    if isinstance(sound, np.ndarray):
-        data = sound
-        if sr < 44100:
-            import warnings
-            msg = '\nWarning: VAD works best with sample rates above '+\
-                '44100 Hz. Therefore, audio will be sampled / resampled from '+\
-                    ' {} to 44100 Hz.'.format(sr)
-            warnings.warn(msg)
-            data, sr = pyso.dsp.resample_audio(data, sr, 44100)
-    else:
-        if sr < 44100:
-            import warnings
-            msg = '\nWarning: VAD works best with sample rates above '+\
-                '44100 Hz. Therefore, audio will be sampled / resampled from '+\
-                    ' {} to 44100 Hz.'.format(sr)
-            warnings.warn(msg)
-            sr = 44100
-        data, sr2 = pyso.loadsound(sound, sr=sr)
-        assert sr2 == sr
-    frame_length = pyso.dsp.calc_frame_length(win_size_ms, sr)
-    if percent_overlap > 0:
-        import warnings
-        msg = 'For VAD calculation, no overlap applied. '+\
-            'Therefore, `percent_overlap` set to 0.'
-        warnings.warn(msg)
-        percent_overlap = 0
-    num_overlap_samples = int(frame_length * percent_overlap)
-    num_subframes = pyso.dsp.calc_num_subframes(len(data),
-                                                frame_length = frame_length,
-                                                overlap_samples = num_overlap_samples,
-                                                zeropad = True)
-    
-    # set number of subframes for extending window
-    extwin_num_samples = pyso.dsp.calc_frame_length(extend_window_ms, sr)
-    num_win_subframes = pyso.dsp.calc_num_subframes(extwin_num_samples,
-                                                    frame_length = frame_length,
-                                                    overlap_samples = num_overlap_samples,
-                                                    zeropad = True)
-    
-    total_rows = fft_bins
-    stft_matrix = pyso.dsp.create_empty_matrix((num_subframes, total_rows),
-                                              complex_vals = True)
-    
-    vad_matrix, (sr, e, f, sfm) = pyso.dsp.vad(data, sr, 
-                                               win_size_ms = win_size_ms,
-                                               percent_overlap = percent_overlap, 
-                                               use_beg_ms = use_beg_ms)
-    vad_matrix_extwin = vad_matrix.copy()
-    
-    # extend VAD windows with 1s
-    if extend_window_ms > 0:
-        for i, row in enumerate(vad_matrix):
-            if row > 0:
-                # label samples before VAD as VAD
-                if i > num_win_subframes:
-                    vad_matrix_extwin[i-num_win_subframes:i] = 1
-                else:
-                    vad_matrix_extwin[:i] = 1
-                # label samples before VAD as VAD
-                if i + num_win_subframes < len(vad_matrix):
-                    vad_matrix_extwin[i:num_win_subframes+i] = 1
-                else:
-                    vad_matrix_extwin[i:] = 1
-                    
-    section_start = 0
-    extra_rows = 0
-    window_frame = pyso.dsp.create_window(window, frame_length)
-    row = 0
-    for frame in range(num_subframes):
-        vad = vad_matrix_extwin[frame]
-        if vad > 0:
-            section = data[section_start:section_start+frame_length]
-            section = pyso.dsp.apply_window(section, 
-                                            window_frame, 
-                                            zeropad = True)
-            section_fft = pyso.dsp.calc_fft(section, 
-                                            real_signal = real_signal,
-                                            fft_bins = total_rows,
-                                            )
-            stft_matrix[row] = section_fft
-            row += 1
-        else:
-            extra_rows += 1
-        section_start += (frame_length - num_overlap_samples)
-    stft_matrix = stft_matrix[:-extra_rows]
-    return stft_matrix[:,:fft_bins//2], sr
-
-    
 def get_mean_freq(sound, sr=16000, win_size_ms = 50, percent_overlap = 0.5,
                           real_signal = False, fft_bins = 1024, 
                           window = 'hann', percent_vad=0.75):
@@ -2595,7 +2452,7 @@ def f0_approximation(sound, sr, low_freq = 50, high_freq = 300, **kwargs):
     else:
         data, sr2 = pyso.loadsound(sound, sr=sr)
         assert sr2 == sr
-    stft = pyso.dsp.get_vad_stft(data, sr=sr, **kwargs)
+    stft = pyso.feats.get_vad_stft(data, sr=sr, **kwargs)
     stft = stft[:,low_freq:high_freq]
     power = np.abs(stft)**2
     dom_f = []
