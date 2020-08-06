b9aefc3f7a1dbce0a353cf1bf5a57f2a8cfad70c
a-n-rose <rose.aislyn.noelle@gmail.com>
2020-08-06 15:18:30 +0200

    testing different settings

--- testing_generator_dataextraction.py
+++ testing_generator_dataextraction.py
@@ -4,18 +4,19 @@ import matplotlib.pyplot as plt
 feature_type = 'stft'
 num_filters = 40
 mono = True
-rate_of_change = False
+rate_of_change = True
 rate_of_acceleration = False
 subtract_mean = False
-use_scipy = True
+use_scipy = False
 dur_sec = 1
 win_size_ms = 25
 percent_overlap = 0.5
-sr = 48000
+sr = 22050
 fft_bins = None
 center = False 
 mode = 'reflect'
 real_signal = False
+
 
 get_feats_kwargs = dict(feature_type = feature_type,
                         num_filters = num_filters,
@@ -103,8 +104,7 @@ augment_settings_dict['speed_decrease']['perc'] = 0.1
 audiodata_path = '../mini-audio-datasets/speech_commands/'
 #augmentation_harmondist.update(
     #dict(augment_settings_dict=augment_settings_dict))
-augment_dict_list = [augmentation_vtlp,
-                     augmentation_none, 
+augment_dict_list = [augmentation_none, 
                      augmentation_pitchdown_novtlp,
                      augmentation_speedup]
 labeled_data = True 
