80ceff7eff6560f337e9b26de5f670b30368f58d
a-n-rose <rose.aislyn.noelle@gmail.com>
2021-01-24 17:01:58 +0100

    typo type() --> isinstance()

--- soundpy/dsp.py
+++ soundpy/dsp.py
@@ -3060,7 +3060,7 @@ def f0_approximation(sound, sr, low_freq = 50, high_freq = 300, **kwargs):
         assert sr2 == sr
     stft = sp.feats.get_vad_stft(data, sr=sr, **kwargs)
     # get_vad_stft now returns stft, vad_matrix but used to return just stft
-    if type(stft, tuple):
+    if isinstance(stft, tuple):
         stft = stft[0]
     stft = stft[:,low_freq:high_freq]
     power = np.abs(stft)**2
